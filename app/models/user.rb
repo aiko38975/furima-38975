aef9dc57c78f2d1af073db90157c9db23007b977
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-03-08 22:24:26 +0900

    user/modelにバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,
+    presence: true
+  validates :password,
+    presence: true,
+    format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i},
+    length: { minimum: 6 }
+  validates :first_name,
+    presence: true,
+    format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name,
+    presence: true,
+    format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name_reading,
+    presence: true,
+    format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_reading,
+    presence: true,
+    format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :birthday,
+    presence: true
 end
