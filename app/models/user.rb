5605fc903f4a9ac24442411d628f01c444a8204f
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-03-08 22:43:29 +0900

    バリデーション追加修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,7 @@ class User < ApplicationRecord
   validates :nickname,
     presence: true
   validates :password,
-    presence: true,
-    format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i},
-    length: { minimum: 6 }
+    format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   validates :first_name,
     presence: true,
     format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
