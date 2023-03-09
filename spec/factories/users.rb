93838ecc9a4249008e0b86043397a6c2e952ec02
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-03-09 18:25:46 +0900

    userモデル単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname               {'テスト'}
+    email                  {'test@example'}
+    password               {'1111ww'}
+    password_confirmation  {password}
+    first_name             {'太郎'}
+    last_name              {'山田'}
+    first_name_reading     {'タロウ'}
+    last_name_reading      {'ヤマダ'}
+    birthday               {'2000-01-01'}
+  end
+end
