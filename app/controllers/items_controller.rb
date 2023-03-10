020968964e33ff4cfb044e85af0a0525490924cb
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-03-10 17:17:59 +0900

    プルダウンとjavaの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def index
+
   end
 
   def new
@@ -10,6 +11,19 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id,:shipping_fee_id, :shipping_pref_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,26 @@
 class Item < ApplicationRecord
+
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
+  validates :category_id, presence: true,
+             numericality: { other_than: 1 } 
   validates :condition_id, presence: true
   validates :shipping_fee_id, presence: true
   validates :shipping_pref_id, presence: true
   validates :shipping_date_id, presence: true
-  validates :price, presence: true
+  validates :price, presence: true,
+            #  format: { with: /\A[0-9]+\z/ },
+             numericality: { only_integer: true,　greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
 
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :shipping_pref
+  belongs_to :shipping_date
 end
+
+
