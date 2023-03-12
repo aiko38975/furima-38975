a4e101ebbcc54f3409c8588323539d323472189b
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-03-12 13:57:05 +0900

    エラーハンドリングの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,14 +3,17 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :category_id, presence: true,
-             numericality: { other_than: 1 } 
-  validates :condition_id, presence: true
-  validates :shipping_fee_id, presence: true
-  validates :shipping_pref_id, presence: true
-  validates :shipping_date_id, presence: true
+             numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, presence: true,
+             numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_fee_id, presence: true,
+  numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_pref_id, presence: true,
+  numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_date_id, presence: true,
+  numericality: { other_than: 1 , message: "can't be blank"} 
   validates :price, presence: true,
-            #  format: { with: /\A[0-9]+\z/ },
-             numericality: { only_integer: true,　greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+             numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
 
   belongs_to :user
   has_one_attached :image
