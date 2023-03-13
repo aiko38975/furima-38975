3261687cdd48c3ac7b0b395e0f4ab1abf9551046
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-03-13 16:36:49 +0900

    item#editアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,9 +26,11 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
 
-    return if user_signed_in? && current_user.id == @item.user_id
-
-    redirect_to action: :index
+    if user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def update
