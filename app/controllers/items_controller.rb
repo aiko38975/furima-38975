cf4f10d7e83873983178a9376845f40853299388
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-03-13 16:03:44 +0900

    robocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,9 +26,9 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
 
-    unless user_signed_in? && current_user.id == @item.user_id 
-      redirect_to action: :index
-    end
+    return if user_signed_in? && current_user.id == @item.user_id
+
+    redirect_to action: :index
   end
 
   def update
@@ -40,7 +40,6 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
-
 
   private
 
