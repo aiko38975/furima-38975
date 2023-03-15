e8522130895710c0f035f0d53e66fb5e3f4e1e83
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-03-15 14:29:36 +0900

    フォームの実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,25 @@
 class PurchasesController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
+  end
+
+  
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    # if @purchase_address.valid?
+      # @purchase_address.save
+      # redirect_to root_path
+    # else
+      # render :new  #new？？？？
+    # end
+  end
+
+  private
+
+  def purchase_params  #mergeはuerだけ？itemは？
+    params.require(:purchase_address).permit(:postal_code, :shipping_pref_id, :city, :street, :building, :phone_number).merge(user_id: current_user.id)
+  end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -7,7 +7,7 @@ class PurchaseAddress
     validates :shipping_pref_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :street
-    validates :phone_number, format: {/\A\d{10,11}\z/}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/}
     validates :user_id
     validates :item_id
   end
