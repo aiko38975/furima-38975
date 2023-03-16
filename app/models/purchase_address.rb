bf5802936fefbaeb56e5d3dae9baa19201f97d04
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-03-16 21:55:53 +0900

    修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,7 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :shipping_pref_id, :city, :street, :building, :phone_number, :user_id, :item_id
-  attr_accessor :token
+  attr_accessor :postal_code, :shipping_pref_id, :city, :street, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
